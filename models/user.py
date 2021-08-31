f21d424257265c553bf7cca9ab540e2586908883
Rohit Gundakalli <rohit.gundakalli@wowlabz.com>
2021-08-31 15:36:31 +0530

    resolving conflict

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 from pydantic import EmailStr
 from .login import Login
 
-<<<<<<< HEAD
 class UserInDB(Login):
     firstName: str
     lastName: str
@@ -10,17 +9,3 @@ class UserInDB(Login):
 
 class User(UserInDB):
     confirmPassword: str
-=======
-class User(BaseModel):
-    fullName: str
-    mobile: int 
-    email:  EmailStr
-    username: str
-    password:str
-
-class UserIn(User):
-    confirm_password: str
-
-class UserInDB(User):
-    pass
->>>>>>> 80ffbe90bd239538b9c647ab2328d3e188451168
