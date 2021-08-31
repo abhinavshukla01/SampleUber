f7546336555147e14001df341bfd329194eef4a2
Abhinav Shukla <abhinav.shukla@wowlabz.com>
2021-08-31 16:47:42 +0530

    Added tag in welcome route

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Depends
 from models.user import UserInDB
 from routers.UserApi import router as userRouter
 from routers.DriverApi import router as driverRouter
-from dependencies import get_current_user#,temp
+from dependencies import get_current_user
+
 
 app = FastAPI(title="Sample Uber")
 app.include_router(userRouter)
 app.include_router(driverRouter)
 
-@app.get("/", tags="Welcome")
-def welcome(request: Request):
-    url = app.url_path_for("register")
-    return {"message": url}
+
+@app.get("/", tags=["Welcome"])
+def welcome(get_user:str = Depends(get_current_user)):
+    return {"message": "Welcome to our app!!"}
